87ad8d4d1b86c148be828b5d0186378417302abe
KYLE <minecraft.kyle.train@gmail.com>
2021-08-30 03:53:40 +0800

    fix

--- colored.v
+++ colored.v
@@ -13,39 +13,39 @@ const (
 )
 
 pub fn reset() string {
-	return .ansi_reset
+	return ansi_reset
 }
 
 pub fn black(str string) string {
-	return .ansi_black + str
+	return ansi_black + str
 }
 
 pub fn red(str string) string {
-	return .ansi_red + str
+	return ansi_red + str
 }
 
 pub fn green(str string) string {
-	return .ansi_green + str
+	return ansi_green + str
 }
 
 pub fn yellow(str string) string {
-	return .ansi_yellow + str
+	return ansi_yellow + str
 }
 
 pub fn blue(str string) string {
-	return .ansi_blue + str
+	return ansi_blue + str
 }
 
 pub fn purple(str string) string {
-	return .ansi_purple + str
+	return ansi_purple + str
 }
 
 pub fn cyan(str string) string {
-	return .ansi_cyan + str
+	return ansi_cyan + str
 }
 
 pub fn white(str string) string {
-	return .ansi_white + str
+	return ansi_white + str
 }
 
 const (
@@ -60,33 +60,33 @@ const (
 )
 
 pub fn bg_black(str string) string {
-	return .ansi_black_bg + str
+	return ansi_black_bg + str
 }
 
 pub fn bg_red(str string) string {
-	return .ansi_red_bg + str
+	return ansi_red_bg + str
 }
 
 pub fn bg_green(str string) string {
-	return .ansi_green_bg + str
+	return ansi_green_bg + str
 }
 
 pub fn bg_yellow(str string) string {
-	return .ansi_yellow_bg + str
+	return ansi_yellow_bg + str
 }
 
 pub fn bg_blue(str string) string {
-	return .ansi_blue_bg + str
+	return ansi_blue_bg + str
 }
 
 pub fn bg_purple(str string) string {
-	return .ansi_purple_bg + str
+	return ansi_purple_bg + str
 }
 
 pub fn bg_cyan(str string) string {
-	return .ansi_cyan_bg + str
+	return ansi_cyan_bg + str
 }
 
 pub fn bg_white(str string) string {
-	return .ansi_white_bg + str
+	return ansi_white_bg + str
 }
